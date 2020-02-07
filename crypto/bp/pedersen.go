7665099e1a7167e3fec9a1bb7e37c691410cfa4f
actuallyachraf <actuallyachraf@gmail.com>
2020-02-07 19:43:12 +0100

    add singular pedersen commitment

--- crypto/bp/pedersen.go
+++ crypto/bp/pedersen.go
@@ -5,6 +5,10 @@ import "github.com/actuallyachraf/algebra/ec"
 import "github.com/actuallyachraf/algebra/nt"
 
 // PedersenCommitment implements the pedersen commitment scheme over elliptic curves.
-func PedersenCommitment(value *nt.Integer) (ec.Point, *nt.Integer) {
-	return ec.Point{}, nil
+func PedersenCommitment(params Parameters, value *nt.Integer) (*ec.Point, *nt.Integer) {
+
+	hidingVal, _ := params.EC.F.Rand()
+	commitment := params.EC.DoubleScalarMult(&params.G, &params.H, value, hidingVal.Big())
+
+	return commitment, hidingVal.Big()
 }
